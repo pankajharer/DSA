962f20e8f2d50d9104c4c738585b5f0271a55df1
PankajHarer <pankajharer2003@gmail.com>
2023-12-25 19:57:21 +0530

    New Commit

--- DSA/Basic Codes/Complementcpp.cpp
+++ DSA/Basic Codes/Complementcpp.cpp
@@ -1,13 +1,33 @@
 #include<iostream>
 using namespace std;
 
+int Compliment(int num)
+{
+    int m=num;
+    int mask=0;
+    
+    if(num==0)
+    {
+        return 1;
+    }
+
+    while(m!=0)
+    {
+       mask= (mask << 1) | 1;
+       m=m>>1;
+    }
+
+    return ~num & mask;
+}
+
+
 int main()
 {
     int num;
     cout<<"Enter the Number:";
     cin>>num;
 
-    cout<<~num<<"";
+    cout<<Compliment(num)<<"";
 
     return 0;
 
